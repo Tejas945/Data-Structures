a361a0f146d164a11bf3da37eab26ac9923f5f9d
Tejas945 <43914911+Tejas945@users.noreply.github.com>
2018-10-24 23:58:37 -0700

    Create stockMain.cpp

---
+++ StockDB Object/stockMain.cpp
@@ -0,0 +1,18 @@
+
+#include <iostream>
+#include "stockDB.h"
+
+using namespace std;
+
+void stocktest();
+void stockNodetest();
+void stockDBtest();
+
+int main()
+{
+//	stocktest();
+//	stockNodetest();
+	stockDBtest();
+
+}
+
