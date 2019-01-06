61feb9ec8aca8eeaaf1b7f51f9a52ab7831175f9
Tejas945 <43914911+Tejas945@users.noreply.github.com>
2019-01-05 23:38:39 -0800

    Create cashier.cpp

---
+++ Customer_Queue/cashier.cpp
@@ -0,0 +1,18 @@
+/*
+ * cashier.cpp
+ *
+ *  Created on: 13-Dec-2018
+ *      Author: Tejas Madappa
+ */
+
+#include "cashier.h"
+#include "customer.h"
+
+bool isbusy()
+{
+
+	cout<<"====is busy====="<<endl;
+
+}
+
+
