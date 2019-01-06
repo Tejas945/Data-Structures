5a7650f00412afbe776d6222d2e876ea5199a740
Tejas945 <43914911+Tejas945@users.noreply.github.com>
2019-01-05 23:36:35 -0800

    Create main.cpp

---
+++ Customer_Queue/main.cpp
@@ -0,0 +1,17 @@
+/*
+ * main.cpp
+ *
+ *  Created on: 13-Dec-2018
+ *    Function: Implement a checkout counter management system
+ *      Author: Tejas Madappa
+ */
+void simulation();
+//void statistics();
+int main()
+{
+	simulation();
+
+}
+
+
+
