b1de1915b2d5956e7c47f3e25b53a511cdb774d4
Tejas Madappa <43914911+Tejas945@users.noreply.github.com>
2020-03-17 19:59:40 -0700

    Create letters_in_string.py

---
+++ Python/Hashmap/letters_in_string.py
@@ -0,0 +1,11 @@
+name = "Pythonmasternumberone"
+d = {}
+for c in name:
+    if c not in d:
+        d[c]=1
+    else:
+        d[c] +=1
+  
+  print(d)      
+# =====Output========
+#{'P': 1, 'y': 1, 't': 2, 'h': 1, 'o': 2, 'n': 3, 'm': 2, 'a': 1, 's': 1, 'e': 3, 'r': 2, 'u': 1, 'b': 1}
